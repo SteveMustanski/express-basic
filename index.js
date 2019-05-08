29f00c3bed1c51ec054f077c3a2c7831a0ef8ff0
SteveMustanski <steve.mustanski@gmail.com>
2019-05-08 12:40:18 -0400

    added draft get, post, put, and delete routes

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import data from './data/data.json';
 
 const app = express();
 const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send(`GET request with / on port ${port}`);
+});
+app.post('/newItem', (req, res) => {
+  res.send(`POST request with /newItem on port ${port}`);
+});
+app.put('/item', (req, res) => {
+  res.send(`PUT request with /newItem on port ${port}`);
+});
+app.delete('/item', (req, res) => {
+  res.send(`DELETE request with /newItem on port ${port}`);
+});
 
 app.listen(port, () => {
   console.log(`Application is listening on port ${port}`);
