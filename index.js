a5bdc2f24c5ed4ee46ad6d251524cbea75329983
SteveMustanski <steve.mustanski@gmail.com>
2019-05-09 13:13:02 -0400

    added redirect example

--- index.js
+++ index.js
@@ -12,10 +12,17 @@ app.get('/', (req, res) => {
   res.json(data);
 });
 
-app.get('/item/:id', (req, res) => {
-  let user = Number(req.params.id);
-  res.send(data[user]);
-});
+app.get(
+  '/item/:id',
+  (req, res, next) => {
+    let user = Number(req.params.id);
+    res.send(data[user]);
+    next();
+  },
+  (req, res) => {
+    console.log('This was executed as next was called.');
+  },
+);
 
 app.post('/newItem', (req, res) => {
   res.send(`POST request with /newItem on port ${port}`);
@@ -23,6 +30,10 @@ app.post('/newItem', (req, res) => {
 
 app.put('/item', (req, res) => {
   res.send(`PUT request with /newItem on port ${port}`);
+});
+
+app.get('/redirect', (req, res) => {
+  res.redirect('http://linkedin.com');
 });
 
 app.delete('/item', (req, res) => {
